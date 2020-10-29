03921ec796383d0f90cccf8c668802bd942ef15a
kschmitz123 <kathrin_schmitz93@gmx.de>
2020-10-29 08:41:51 +0100

    Render components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Welcome from "./Components";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Welcome name="Kathrin"></Welcome>
     </div>
   );
 }
